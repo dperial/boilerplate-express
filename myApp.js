ec70fc1d9697e3ab86449c6942b359d2aae7f6a9
p.kuaguim-nguefack <p.kuaguim-nguefack@mmkh.de>
2025-07-03 00:49:11 +0200

    feat: print hello world in the console and send it to the serve

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
-
-
+// Get Method to log "Hello Express" in the console"
+app.get('/', (req, res) => {
+    console.log("Hello Express"); // Log Hello Express in the console
+    // Send a response to the client
+    res.send("Hello Express");
+});
 
 
 
