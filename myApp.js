cf861c84a19d8d861777f88c93ad7b5fe5792d59
p.kuaguim-nguefack <p.kuaguim-nguefack@mmkh.de>
2025-07-03 00:55:52 +0200

    feat: serve an HTML file (view/index.html)

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 let express = require('express');
+const path = require('path');
 let app = express();
 // Get Method to log "Hello Express" in the console"
 app.get('/', (req, res) => {
     console.log("Hello Express"); // Log Hello Express in the console
     // Send a response to the client
-    res.send("Hello Express");
+    res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
 
