c685e7da62b73f1ac5223fa181c1085b58bcd192
p.kuaguim-nguefack <p.kuaguim-nguefack@mmkh.de>
2025-07-03 01:18:58 +0200

    feat: REST API, serve a JSON on a specific route

--- myApp.js
+++ myApp.js
@@ -4,9 +4,14 @@ let app = express();
 // Get Method to log "Hello Express" in the console"
 app.get('/', (req, res) => {
     console.log("Hello Express"); // Log Hello Express in the console
-    // Send a response to the client
+    // Send a HTML file to the client
     res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 // Serve static files from the "public" directory
 app.use('/public', express.static(path.join(__dirname , 'public')));
+
+// Simple API endpoint to return a JSON object
+app.get('/json', (req, res) => {
+    res.json({ message: "Hello json" }); // Return a JSON object
+})
 module.exports = app;
