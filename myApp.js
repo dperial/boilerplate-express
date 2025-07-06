196e6d79604a2dda0161c976f8009f79f5445506
p.kuaguim-nguefack <p.kuaguim-nguefack@mmkh.de>
2025-07-07 00:27:55 +0200

    feat: create a simple logger

--- myApp.js
+++ myApp.js
@@ -3,10 +3,16 @@ const path = require('path');
 require('dotenv').config();
 
 let app = express();
+// Build a simple logger middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`); // Log the request method, path, and IP address
+    next(); // Call the next middleware in the stack
+});
+
 // Serve static files from the public directory
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-// Get Method to log "Hello Express" in the console"
+// Add a route to serve the index.html fil
 app.get('/', (req, res) => {
     console.log("Hello Express"); // Log Hello Express in the console
     // Send a HTML file to the client
@@ -21,5 +27,5 @@ app.get('/json', (req, res) => {
     } else {
         res.json({ message: "Hello json" }); // Return a JSON object
     }
-})
+});
 module.exports = app;
